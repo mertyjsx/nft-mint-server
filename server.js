83ed871123fe7f8163070d517475d1d49b0ded68
mertyjsx <orkunmertgs41@gmail.com>
2022-02-21 05:04:26 +0300

    s

--- server.js
+++ server.js
@@ -8,12 +8,16 @@ app.use(cors());
 app.use(express.json());
 
 //routes
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
-app.use("/authentication", require("./routes/jwtAuth"));
-app.use("/admin", require("./routes/admin"));
 
-app.use("/dashboard", require("./routes/dashboard"));
+app.use("/mint_nft", require("./routes/nft"));
 
 app.listen(5000, () => {
+
   console.log(`Server is starting on port 5000`);
 });
